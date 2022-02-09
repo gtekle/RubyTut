4bf5b1e967a80849978b097b2d09f08fbe53ecd7
tYohannes <teklegy@gmail.com>
2022-02-09 21:18:31 +0300

    comments

--- 1-outputs.rb
+++ 1-outputs.rb
@@ -6,3 +6,14 @@ print "world"
 
 p "Hello "
 p "world"
+
+# This is a single line comment
+
+# A block of code can be commented out by
+# starting each line with a hash-symbol
+
+=begin
+puts "Hello"
+Anything written here will be commented out. So,
+this the second method of commenting out a block of text  
+=end
